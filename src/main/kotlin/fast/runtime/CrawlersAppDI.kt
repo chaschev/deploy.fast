3aebff1b05f8df6b2c8581c540a8231910cbfd42
chaschev <chaschev@yandex.ru>
2018-05-06 20:42:49 +0800

    refactor

---
+++ src/main/kotlin/fast/runtime/CrawlersAppDI.kt
@@ -0,0 +1,47 @@
+package fast.runtime
+
+import fast.api.DeployFastApp
+import fast.inventory.Group
+import fast.inventory.Host
+import fast.inventory.Inventory
+import fast.runtime.DeployFastDI.FAST
+import org.kodein.di.*
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.singleton
+
+
+object CrawlersAppDI {
+  init {
+    DeployFastDI.FAST = Kodein {
+      extend(FAST)
+
+      bind<Inventory>() with singleton {
+        Inventory(
+          listOf(
+            Group(
+              name = "vpn",
+              hosts = listOf(
+                Host("vpn1"),
+                Host("vpn2")
+              )
+            ),
+            Group(
+              name = "vm",
+              hosts = listOf(
+                Host("192.168.5.10")
+              )
+            )
+          )
+        ).init()
+      }
+
+      bind<DeployFastApp<*>>() with singleton { CrawlersFastApp() }
+
+      bind("dsl") from singleton { CrawlersFastApp.dsl() }
+
+      bind("runAt") from singleton { "vm" }
+    }
+  }
+
+}
+
